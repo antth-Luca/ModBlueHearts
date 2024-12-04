a2cc909b98e58e54088c0e93de4fbc09ab1af9d4
antthLuca <123425103+antth-Luca@users.noreply.github.com>
2024-12-04 12:42:32 -0300

    Conclusão Aula #0

---
+++ projeto/BlueHearts/src/main/java/io/github/antthluca/blue_hearts/BlueHearts.java
@@ -0,0 +1,8 @@
+package io.github.antthluca.blue_hearts;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(BlueHearts.MODID)
+public class BlueHearts {
+    public static final String MODID = "blue_hearts";
+}
