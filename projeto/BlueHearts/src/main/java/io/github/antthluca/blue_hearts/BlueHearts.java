8c28194d87ae6412c56fa5d40880c97279818b05
antthLuca <123425103+antth-Luca@users.noreply.github.com>
2024-12-05 12:45:01 -0300

    Registro de um item: Não terminado!

--- projeto/BlueHearts/src/main/java/io/github/antthluca/blue_hearts/BlueHearts.java
+++ projeto/BlueHearts/src/main/java/io/github/antthluca/blue_hearts/BlueHearts.java
@@ -1,8 +1,16 @@
 package io.github.antthluca.blue_hearts;
 
+import io.github.antthluca.blue_hearts.init.InitItems;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod(BlueHearts.MODID)
 public class BlueHearts {
     public static final String MODID = "blue_hearts";
+
+    public BlueHearts() {
+        IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
+        InitItems.ITEMS.register(bus);
+    }
 }
---
+++ projeto/BlueHearts/src/main/java/io/github/antthluca/blue_hearts/init/InitItems.java
@@ -0,0 +1,19 @@
+package io.github.antthluca.blue_hearts.init;
+
+import io.github.antthluca.blue_hearts.BlueHearts;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class InitItems {
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(
+        ForgeRegistries.ITEMS, BlueHearts.MODID
+    );
+
+    // Items
+    public static final RegistryObject<Item> VITAL_SAP = ITEMS.register(
+        "vital_sap", () -> new Item(new Item.Properties()
+            .tab(CreativeModeTab.TAB_MISC)));
+}
