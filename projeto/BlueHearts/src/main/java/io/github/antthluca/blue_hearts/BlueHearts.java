0cc2883c67ff08c795eddf68f140b9c622b8b8b6
antthLuca <123425103+antth-Luca@users.noreply.github.com>
2024-12-08 15:21:45 -0300

    Receita: A Seiva Vital funciona!

--- projeto/BlueHearts/src/main/java/io/github/antthluca/blue_hearts/BlueHearts.java
+++ projeto/BlueHearts/src/main/java/io/github/antthluca/blue_hearts/BlueHearts.java
@@ -3,8 +3,10 @@ package io.github.antthluca.blue_hearts;
 import io.github.antthluca.blue_hearts.init.InitEffects;
 import io.github.antthluca.blue_hearts.init.InitItems;
 import io.github.antthluca.blue_hearts.init.InitPotions;
+import io.github.antthluca.blue_hearts.init.InitRecipes;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod(BlueHearts.MODID)
@@ -17,5 +19,11 @@ public class BlueHearts {
         InitItems.ITEMS.register(bus);
         InitEffects.MOB_EFFECTS.register(bus);
         InitPotions.POTIONS.register(bus);
+
+        bus.addListener(this::setup);
+    }
+
+    private void setup(final FMLCommonSetupEvent event) {
+        event.enqueueWork(InitRecipes::registerBrewingRecipes);
     }
 }
