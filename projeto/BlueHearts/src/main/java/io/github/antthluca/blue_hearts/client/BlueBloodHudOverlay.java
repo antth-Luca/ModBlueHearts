fd9d94f80ab6f0c2626758734cb133d3e294323d
Luca Anthony <123425103+antth-Luca@users.noreply.github.com>
2024-12-12 14:46:42 -0300

    Update BlueBloodHudOverlay.java

--- projeto/BlueHearts/src/main/java/io/github/antthluca/blue_hearts/client/BlueBloodHudOverlay.java
+++ projeto/BlueHearts/src/main/java/io/github/antthluca/blue_hearts/client/BlueBloodHudOverlay.java
@@ -13,24 +13,25 @@ public class BlueBloodHudOverlay {
     private static final ResourceLocation MIDDLE_BLUE_HEART = new ResourceLocation(BlueHearts.MODID, "textures/hud/middle_blue_heart.png");
 
     public static final IIngameOverlay HUD_BLUE_BLOOD = ((gui, poseStack, partialTick, width, height) -> {
-        int x = width / 2; // Centraliza os corações
-        int y = height;    // Define a altura
+        int x = width / 2 - 91; // Centraliza os corações
+        int y = height - 39;    // Define a altura
     
         RenderSystem.setShader(GameRenderer::getPositionTexShader);
         RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
     
-        int blueBlood = ClientBlueBloodData.getPlayerBlueBlood(); // Obtém o valor atual do blue blood
+        int blueBlood = ClientBlueBloodData.getPlayerBlueBlood();
+        int maxBlueBlood = ClientBlueBloodData.getPlayerMaxBlueBlood();
     
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < maxBlueBlood; i++) {
             if (blueBlood > i) {
                 if (blueBlood - i > 0.5) {
                     // Renderiza o coração cheio
                     RenderSystem.setShaderTexture(0, FULL_BLUE_HEART);
-                    GuiComponent.blit(poseStack, x - 94 + (i * 9), y - 54, 0, 0, 12, 12, 12, 12);
+                    GuiComponent.blit(poseStack, x + (i * 9), y - 54, 0, 0, 12, 12, 12, 12);
                 } else {
                     // Renderiza o meio coração
                     RenderSystem.setShaderTexture(0, MIDDLE_BLUE_HEART);
-                    GuiComponent.blit(poseStack, x - 94 + (i * 9), y - 54, 0, 0, 12, 12, 12, 12);
+                    GuiComponent.blit(poseStack, x + (i * 9), y - 54, 0, 0, 12, 12, 12, 12);
                 }
             } else {
                 break; // Sai do loop se não há mais blue blood a renderizar
