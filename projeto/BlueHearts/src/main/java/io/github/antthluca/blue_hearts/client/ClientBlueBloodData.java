72173dcd4e13a01ae5b9a04543a8fa4280cf1d52
Luca Anthony <123425103+antth-Luca@users.noreply.github.com>
2024-12-12 14:50:25 -0300

    Update ClientBlueBloodData.java

--- projeto/BlueHearts/src/main/java/io/github/antthluca/blue_hearts/client/ClientBlueBloodData.java
+++ projeto/BlueHearts/src/main/java/io/github/antthluca/blue_hearts/client/ClientBlueBloodData.java
@@ -1,13 +1,22 @@
 package io.github.antthluca.blue_hearts.client;
 
 public class ClientBlueBloodData {
-    private static int playerBlueBlood;
+    private static int playerBlueBlood = 0;
+    private static int maxBlueBlood = 0;
 
-    public static void set(int blueBlood) {
+    public static void setPlayerBlueBlood(int blueBlood) {
         ClientBlueBloodData.playerBlueBlood = blueBlood;
     }
 
     public static int getPlayerBlueBlood() {
         return playerBlueBlood;
+    }
+
+    public static void setMaxBlueBlood(int blueBlood) {
+        ClientBlueBloodData.maxBlueBlood = blueBlood;
+    }
+
+    public static int getMaxBlueBlood() {
+        return maxBlueBlood;
     }
 }
