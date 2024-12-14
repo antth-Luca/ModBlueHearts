346802770c50ca403f3f34fd08d70118bbfd1cd2
antthLuca <123425103+antth-Luca@users.noreply.github.com>
2024-12-14 11:30:22 -0300

    Restaurar corações ao dormir: Ok!

---
+++ projeto/BlueHearts/src/main/java/io/github/antthluca/blue_hearts/handlers/SleepHandler.java
@@ -0,0 +1,21 @@
+package io.github.antthluca.blue_hearts.handlers;
+
+import io.github.antthluca.blue_hearts.capabilities.PlayerBlueBloodProvider;
+import net.minecraft.world.entity.player.Player;
+import net.minecraftforge.event.entity.player.PlayerWakeUpEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber
+public class SleepHandler {
+    @SubscribeEvent
+    public static void onPlayerWakeUp(PlayerWakeUpEvent event) {
+        Player player = event.getPlayer();
+        if (!player.level.isClientSide()) { // Garante que é no lado do servidor
+            player.getCapability(PlayerBlueBloodProvider.PLAYER_BLUE_BLOOD).ifPresent(blueBlood -> {
+                int maxBlueBlood = blueBlood.getMAXBlueBlood();
+                blueBlood.setBlueBlood(maxBlueBlood); // Restaura até o máximo
+            });
+        }
+    }
+}
