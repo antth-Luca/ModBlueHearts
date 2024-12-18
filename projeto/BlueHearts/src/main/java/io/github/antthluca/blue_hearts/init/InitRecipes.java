9e53fb3693915ac15030f0edde7d4182a8e58938
antthLuca <123425103+antth-Luca@users.noreply.github.com>
2024-12-18 11:00:08 -0300

    Melhorando receitas de poções e alteração da receita do Sangue Vital

--- projeto/BlueHearts/src/main/java/io/github/antthluca/blue_hearts/init/InitRecipes.java
+++ projeto/BlueHearts/src/main/java/io/github/antthluca/blue_hearts/init/InitRecipes.java
@@ -1,26 +1,24 @@
 package io.github.antthluca.blue_hearts.init;
 
 import net.minecraft.world.item.Items;
-import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.alchemy.PotionUtils;
+import io.github.antthluca.blue_hearts.utils.BetterBrewingRecipe;
 import net.minecraft.world.item.alchemy.Potions;
 import net.minecraftforge.common.brewing.BrewingRecipeRegistry;
-import net.minecraft.world.item.crafting.Ingredient;
 
 public class InitRecipes {
     public static void registerBrewingRecipes() {
         // Blue Blood Potion
-        BrewingRecipeRegistry.addRecipe(
-            Ingredient.of(PotionUtils.setPotion(new ItemStack(Items.POTION), Potions.AWKWARD)), // Poção base
-            Ingredient.of(InitItems.VITAL_SAP.get()), // Ingrediente
-            PotionUtils.setPotion(new ItemStack(Items.POTION), InitPotions.BLUE_BLOOD_POTION.get()) // Resultado
-        );
+        BrewingRecipeRegistry.addRecipe(new BetterBrewingRecipe(
+            Potions.REGENERATION, // Poção base
+            InitItems.VITAL_SAP.get(), // Ingrediente
+            InitPotions.BLUE_BLOOD_POTION.get() // Resultado
+        ));
 
         // Orange Antidote Potion
-        BrewingRecipeRegistry.addRecipe(
-            Ingredient.of(PotionUtils.setPotion(new ItemStack(Items.POTION), Potions.POISON)), // Poção base
-            Ingredient.of(Items.ROTTEN_FLESH), // Ingrediente
-            PotionUtils.setPotion(new ItemStack(Items.POTION), InitPotions.ORANGE_ANTIDOTE_POTION.get()) // Resultado
-        );
+        BrewingRecipeRegistry.addRecipe(new BetterBrewingRecipe(
+            Potions.POISON, // Poção base
+            Items.ROTTEN_FLESH, // Ingrediente
+            InitPotions.ORANGE_ANTIDOTE_POTION.get() // Resultado
+        ));
     }
 }
