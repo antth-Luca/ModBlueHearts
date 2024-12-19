717ce1b7ae7bb11f5fdb58ca582ec3b9f1e468c7
antthLuca <123425103+antth-Luca@users.noreply.github.com>
2024-12-19 19:27:58 -0300

    Arbusto de ponta cabeça

--- projeto/BlueHearts/src/main/java/io/github/antthluca/blue_hearts/blocks/CustomBlocks.java
+++ projeto/BlueHearts/src/main/java/io/github/antthluca/blue_hearts/blocks/CustomBlocks.java
@@ -2,22 +2,28 @@ package io.github.antthluca.blue_hearts.blocks;
 
 import io.github.antthluca.blue_hearts.init.InitFoods;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.util.RandomSource;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
+import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.block.SweetBerryBushBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.gameevent.GameEvent;
 import net.minecraft.world.phys.BlockHitResult;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.VoxelShape;
 
 public class CustomBlocks {
     @SuppressWarnings("null")
@@ -27,12 +33,45 @@ public class CustomBlocks {
         }
 
         @Override
+        public BlockState getStateForPlacement(BlockPlaceContext context) {
+            if (context.getClickedFace() == Direction.DOWN) {
+                return this.defaultBlockState().setValue(AGE, 0); // Substitua AGE pelo seu atributo de estado, se houver.
+            }
+            return null; // Retorna nulo para impedir que seja colocado em superfícies inválidas.
+        }
+
+        @Override
+        public VoxelShape getShape(BlockState state, BlockGetter block, BlockPos pos, CollisionContext context) {
+            if (state.getValue(AGE) == 0) {
+                return Block.box(3.0D, 8.0D, 3.0D, 13.0D, 16.0D, 13.0D);
+            } else {
+                return state.getValue(AGE) < 3 ? Block.box(1.0D, 0.0D, 1.0D, 15.0D, 16.0D, 15.0D) : super.getShape(state, block, pos, context);
+            }
+        }
+
+        @Override
         public ItemStack getCloneItemStack(BlockGetter block, BlockPos pos, BlockState state) {
             return new ItemStack(InitFoods.VITAL_FRUIT.get());
         }
 
         @Override
-        public void entityInside(BlockState p_57270_, Level p_57271_, BlockPos p_57272_, Entity p_57273_) {
+        public boolean canSurvive(BlockState state, LevelReader world, BlockPos pos) {
+            return world.getBlockState(pos.above()).isFaceSturdy(world, pos.above(), Direction.DOWN);
+        }
+
+        @Override
+        public void randomTick(BlockState state, ServerLevel world, BlockPos pos, RandomSource randomSource) {
+            int i = state.getValue(AGE);
+            if (i < 3 && world.getRawBrightness(pos.below(), 0) >= 9 && net.minecraftforge.common.ForgeHooks.onCropsGrowPre(world, pos, state, randomSource.nextInt(5) == 0)) {
+                BlockState blockstate = state.setValue(AGE, Integer.valueOf(i + 1));
+                world.setBlock(pos, blockstate, 2);
+                world.gameEvent(GameEvent.BLOCK_CHANGE, pos, GameEvent.Context.of(blockstate));
+                net.minecraftforge.common.ForgeHooks.onCropsGrowPost(world, pos, state);
+            }
+        }
+
+        @Override
+        public void entityInside(BlockState state, Level level, BlockPos pos, Entity entity) {
             return;  // Não causa dano!
         }
 
