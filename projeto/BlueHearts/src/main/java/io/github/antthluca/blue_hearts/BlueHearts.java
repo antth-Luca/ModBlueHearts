451025f97ea09d82b0aa04d24ee323c482ec95b8
antthLuca <123425103+antth-Luca@users.noreply.github.com>
2024-12-27 10:10:59 -0300

    Exclusão de arquivos de receitas não usados

--- projeto/BlueHearts/src/main/java/io/github/antthluca/blue_hearts/BlueHearts.java
+++ projeto/BlueHearts/src/main/java/io/github/antthluca/blue_hearts/BlueHearts.java
@@ -9,7 +9,6 @@ import io.github.antthluca.blue_hearts.init.InitItems;
 import io.github.antthluca.blue_hearts.init.InitPotions;
 import io.github.antthluca.blue_hearts.init.InitRecipes;
 import io.github.antthluca.blue_hearts.integration.curios.init.InitItemsCurios;
-import io.github.antthluca.blue_hearts.integration.curios.init.InitRecipesCurios;
 import io.github.antthluca.blue_hearts.networking.ModMessages;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.RegisterCommandsEvent;
@@ -48,7 +47,6 @@ public class BlueHearts {
         // Curios
         if (HAS_CURIOS) {
             InitItemsCurios.ITEMS.register(bus);
-            InitRecipesCurios.registerShapelessRecipes();
 
             bus.addListener(this::sendIMC);
         }
