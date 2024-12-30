d1f964bb982543f0d8d184007cbc0f21c352d630
antthLuca <123425103+antth-Luca@users.noreply.github.com>
2024-12-30 08:47:00 -0300

    Slot de 'charm' do Curios, consertado!

--- projeto/BlueHearts/src/main/java/io/github/antthluca/blue_hearts/BlueHearts.java
+++ projeto/BlueHearts/src/main/java/io/github/antthluca/blue_hearts/BlueHearts.java
@@ -11,22 +11,24 @@ import io.github.antthluca.blue_hearts.init.InitRecipes;
 import io.github.antthluca.blue_hearts.integration.curios.init.InitEffectsCurios;
 import io.github.antthluca.blue_hearts.integration.curios.init.InitItemsCurios;
 import io.github.antthluca.blue_hearts.networking.ModMessages;
-import net.minecraft.world.item.CreativeModeTab;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.RegisterCommandsEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.ModList;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import top.theillusivec4.curios.api.SlotTypePreset;
+import top.theillusivec4.curios.api.SlotTypeMessage;
 
 @Mod(BlueHearts.MODID)
 public class BlueHearts {
     public static final String MODID = "blue_hearts";
     public static final boolean HAS_CURIOS = ModList.get().isLoaded("curios");
-    public static CreativeModeTab TAB = null;
 
     public BlueHearts() {
         IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
@@ -41,6 +43,9 @@ public class BlueHearts {
             // Registre itens e efeitos relacionados ao Curios
             InitItemsCurios.ITEMS.register(bus);
             InitEffectsCurios.MOB_EFFECTS.register(bus);
+
+            // Adicionar 1 slot 'charm' aos jogadores
+            bus.addListener(this::sendIMC);
         }
         // Abas do modo criativo
         InitCreativeTabs.TABS.register(bus);
@@ -67,5 +72,10 @@ public class BlueHearts {
 
     private void registerCommands(RegisterCommandsEvent event) {
         BlueHeartsManagerCommands.register(event.getDispatcher());
+    }
+
+    @SuppressWarnings("deprecation")
+    public void sendIMC(InterModEnqueueEvent e) {
+        InterModComms.sendTo("curios", SlotTypeMessage.REGISTER_TYPE, () -> SlotTypePreset.CHARM.getMessageBuilder().build());
     }
 }
--- projeto/BlueHearts/src/main/java/io/github/antthluca/blue_hearts/potions/ItemBasePotion.java
+++ projeto/BlueHearts/src/main/java/io/github/antthluca/blue_hearts/potions/ItemBasePotion.java
@@ -3,11 +3,8 @@ package io.github.antthluca.blue_hearts.potions;
 import java.util.List;
 import java.util.Random;
 
-import org.jetbrains.annotations.Nullable;
-
 import com.google.common.collect.ImmutableList;
 
-import io.github.antthluca.blue_hearts.BlueHearts;
 import net.minecraft.ChatFormatting;
 import net.minecraft.advancements.CriteriaTriggers;
 import net.minecraft.network.chat.Component;
@@ -18,7 +15,6 @@ import net.minecraft.world.InteractionResult;
 import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
@@ -56,11 +52,6 @@ public abstract class ItemBasePotion extends Item {
     @Override
     public void onCraftedBy(ItemStack stack, Level worldIn, Player playerIn) {
         // Existential void
-    }
-
-    @Nullable
-    public CreativeModeTab getCreativeTab() {
-        return BlueHearts.TAB;
     }
 
     public List<ItemStack> getCreativeTabStacks() {
